542a6faa4585c0ec64dbe02ce08da477f825a7b9
David Little <david.frank.little@gmail.com>
2017-12-20 12:00:07 -0500

    Remove build generated files

--- deps/deps.jl
+++ deps/deps.jl
@@ -1,2 +1,2 @@
 const weber_portaudio = "/Users/davidlittle/.julia/v0.6/Homebrew/deps/usr/lib/libportaudio.2.dylib"
-const weber_sound = "/Users/davidlittle/.julia/v0.6/TimedPlayback/deps/usr/lib/libweber-sound.3.dylib"
+const weber_sound = "/Users/davidlittle/MEGA/Research/software/TimedSound/deps/usr/lib/libweber-sound.4.dylib"
